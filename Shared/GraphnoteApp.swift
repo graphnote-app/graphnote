c4e14deb9f1486495e381d43fbe6a4ab40207499
Hayden Pennington <haydenpennington@icloud.com>
2022-01-22 21:14:50 -0600

    Init xcode project

---
+++ Shared/GraphnoteApp.swift
@@ -0,0 +1,17 @@
+//
+//  GraphnoteApp.swift
+//  Shared
+//
+//  Created by Hayden Pennington on 1/22/22.
+//
+
+import SwiftUI
+
+@main
+struct GraphnoteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
