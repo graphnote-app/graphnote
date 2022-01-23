430261aa659deaabf98c5cf8594516d6c3ab7c2b
Hayden Pennington <haydenpennington@icloud.com>
2022-01-22 23:16:39 -0600

    Set macOS minimum window size

--- Shared/GraphnoteApp.swift
+++ Shared/GraphnoteApp.swift
@@ -7,16 +7,35 @@
 
 import SwiftUI
 
+fileprivate enum MacOSDimensions: CGFloat {
+    case windowMinWidth = 1000
+    case windowMinHeight = 600
+}
+
 @main
 struct GraphnoteApp: App {
     var body: some Scene {
         WindowGroup {
+            #if os(macOS)
             GeometryReader { geometry in
                 ContentView()
                     .toolbar {
-                        Text("")
+                        ToolbarItem {
+                            Text("")
+                        }
                     }
+                    
+            }.frame(
+                minWidth: MacOSDimensions.windowMinWidth.rawValue,
+                idealWidth: MacOSDimensions.windowMinWidth.rawValue,
+                minHeight: MacOSDimensions.windowMinHeight.rawValue,
+                idealHeight: MacOSDimensions.windowMinHeight.rawValue
+            )
+            #else
+            GeometryReader { geometry in
+                ContentView()
             }
+            #endif
         }
     }
 }
