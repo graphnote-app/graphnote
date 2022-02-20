8f347421d870c2f56b5767df54dbbc7225c02f1d
Hayden Pennington <haydenpennington@icloud.com>
2022-02-20 12:34:04 -0600

    Add TreeViewSubItem

--- Shared/TreeView/TreeViewItem.swift
+++ Shared/TreeView/TreeViewItem.swift
@@ -20,101 +20,6 @@ enum FocusField: Hashable {
    case field
 }
 
-struct TreeViewItemCell: View {
-    @Environment(\.colorScheme) var colorScheme
-    @Environment(\.managedObjectContext) var moc
-    var title: Binding<String>
-    let documentId: UUID
-    let workspaceId: UUID
-    var selected: Binding<DocumentIdentifier>
-    let deleteDocument: (UUID, UUID) -> ()
-    let refresh: () -> ()
-    @State var editable = false
-    @FocusState private var focusedField: FocusField?
-
-    @ViewBuilder func textOrTextField() -> some View {
-        HStack {
-            if editable {
-                CheckmarkView()
-                    .onTapGesture {
-                       editable = false
-                    }
-                TextField("", text: title)
-                    .onSubmit {
-                        editable = false
-                        focusedField = nil
-                    }
-                    .focused($focusedField, equals: .field)
-                    .onAppear {
-                        print("onAppear")
-                        if editable {
-                            focusedField = .field
-                        }
-                    }
-            } else {
-                BulletView()
-                    .padding(TreeViewItemDimensions.rowPadding.rawValue)
-                HStack {
-                    Text(title.wrappedValue)
-
-                    Spacer()
-                }.frame(width: 130)
-            }
-        }
-    }
-
-    var body: some View {
-        ZStack(alignment: .leading) {
-            EffectView()
-            
-            if selected.wrappedValue == DocumentIdentifier(workspaceId: self.workspaceId, documentId: self.documentId) {
-                
-                self.textOrTextField()
-                    .padding(TreeViewItemDimensions.rowPadding.rawValue)
-                    .background (
-                        colorScheme == .dark ? Color(red: 0.9, green: 0.9, blue: 0.9, opacity: 0.1) : Color(red: 0.1, green: 0.1, blue: 0.1, opacity: 0.1)
-                    )
-                    .cornerRadius(4)
-                    .contextMenu {
-                        Button {
-                            editable = true
-                        } label: {
-                            Text("Rename")
-                        }
-                        Button {
-                            print("Delete document: \(documentId)")
-                            deleteDocument(workspaceId, documentId)
-                            refresh()
-                        } label: {
-                            Text("Delete document")
-                        }
-                    }
-            } else {
-                self.textOrTextField()
-                    .padding(TreeViewItemDimensions.rowPadding.rawValue)
-                    .contextMenu {
-                        Button {
-                            editable = true
-                        } label: {
-                            Text("Rename")
-                        }
-                        Button {
-                            print("Delete document: \(documentId)")
-                            deleteDocument(workspaceId, documentId)
-                            refresh()
-                        } label: {
-                            Text("Delete document")
-                        }
-                    }
-                    
-            }
-        }
-        .onTapGesture {
-            selected.wrappedValue = DocumentIdentifier(workspaceId: workspaceId, documentId: documentId)
-        }
-    }
-}
-
 struct TreeViewItem: View, Identifiable {
     @Environment(\.colorScheme) var colorScheme
     
@@ -228,7 +133,7 @@ struct TreeViewItem: View, Identifiable {
             VStack(alignment: .leading) {
                 if let _ = viewModel.documents {
                     ForEach(0..<viewModel.documents.count, id: \.self) { index in
-                        TreeViewItemCell(title: $viewModel.documents[index].title, documentId: viewModel.documents[index].id, workspaceId: viewModel.documents[index].workspace.id, selected: selected, deleteDocument: viewModel.deleteDocument, refresh: refreshDocuments)
+                        TreeViewSubItem(title: $viewModel.documents[index].title, documentId: viewModel.documents[index].id, workspaceId: viewModel.documents[index].workspace.id, selected: selected, deleteDocument: viewModel.deleteDocument, refresh: refreshDocuments)
                     }
                 }
                 
