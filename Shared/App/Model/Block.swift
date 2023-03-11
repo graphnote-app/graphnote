11314c40eedba099a891c5eb1ab79e8c87507573
Hayden Pennington <haydenpennington@icloud.com>
2023-03-11 14:36:11 -0600

    Add Block model class

---
+++ Shared/App/Model/Block.swift
@@ -0,0 +1,27 @@
+//
+//  Block.swift
+//  Graphnote
+//
+//  Created by Hayden Pennington on 3/11/23.
+//
+
+import Foundation
+import CoreData
+
+public class Block: NSManagedObject {
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<Block> {
+        return NSFetchRequest<Block>(entityName: "Block")
+    }
+    
+    @NSManaged public var id: UUID
+    @NSManaged public var content: String
+    @NSManaged public var createdAt: Date
+    @NSManaged public var modifiedAt: Date
+}
+
+extension Block : Comparable {
+    public static func < (lhs: Block, rhs: Block) -> Bool {
+        lhs.createdAt < rhs.createdAt
+    }
+}
+
