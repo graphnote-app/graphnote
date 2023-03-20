76397db32a7461887d37f91a2275ed9258efd841
Hayden Pennington <haydenpennington@icloud.com>
2023-03-19 23:27:32 -0500

    Move dimesion constants to their own file

--- macOS/App/App.swift
+++ macOS/App/App.swift
@@ -7,16 +7,6 @@
 
 import SwiftUI
 
-struct GlobalDimension {
-    static let treeWidth: CGFloat = 250
-    static let toolbarWidth = Spacing.spacing4.rawValue + (Spacing.spacing3.rawValue * 2)
-    static let maxDocumentContentWidth = 800.0
-    static let minDocumentContentWidth = 400.0
-    static let minDocumentContentHeight = 300.0
-    static let windowDefaultWidth = 1038.0
-    static let windowDefaultHeight = 600.0
-}
-
 @main
 struct GNApp: App {
     var body: some Scene {
---
+++ macOS/App/GlobalDimension.swift
@@ -0,0 +1,18 @@
+//
+//  GlobalDimension.swift
+//  Graphnote (macOS)
+//
+//  Created by Hayden Pennington on 3/19/23.
+//
+
+import Foundation
+
+struct GlobalDimension {
+    static let treeWidth: CGFloat = 250
+    static let toolbarWidth = Spacing.spacing4.rawValue + (Spacing.spacing3.rawValue * 2)
+    static let maxDocumentContentWidth = 800.0
+    static let minDocumentContentWidth = 400.0
+    static let minDocumentContentHeight = 300.0
+    static let windowDefaultWidth = 1038.0
+    static let windowDefaultHeight = 600.0
+}
