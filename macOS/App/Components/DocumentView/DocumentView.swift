2a63c1190d68b0eee62c18d336e947cd53a6adce
Hayden Pennington <haydenpennington@icloud.com>
2023-03-19 22:50:32 -0500

    Cleanup

--- macOS/App/Components/DocumentView/DocumentView.swift
+++ macOS/App/Components/DocumentView/DocumentView.swift
@@ -7,12 +7,7 @@
 
 import SwiftUI
 
-fileprivate let scrollWidth: CGFloat = 16
-fileprivate let pageMinHeightMultiplier = 1.3
-fileprivate let maxBlockWidth: CGFloat = 800
 fileprivate let pad: Double = 40
-fileprivate let textSpacing: Double = 14.0
-fileprivate let toolbarHeight: CGFloat = 28
 
 struct DocumentView: View {
     @Environment(\.colorScheme) private var colorScheme
