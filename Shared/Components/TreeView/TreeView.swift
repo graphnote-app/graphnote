e08ec01f7ffa34c6466e5adaf96fd0ee25029ab4
Hayden Pennington <haydenpennington@icloud.com>
2023-03-21 22:27:23 -0500

    Adjust padding

--- Shared/Components/TreeView/TreeView.swift
+++ Shared/Components/TreeView/TreeView.swift
@@ -18,7 +18,7 @@ struct TreeView: View {
                         TreeViewLabel(id: UUID(), label: .constant(items[i].title), color: items[i].color) {
                             ForEach(items[i].subItems, id: \.id) { subItem in
                                 TreeViewSubline(title: subItem.title)
-                                    .padding(Spacing.spacing2.rawValue)
+                                    .padding(Spacing.spacing1.rawValue)
                             }
                         }
                     }
