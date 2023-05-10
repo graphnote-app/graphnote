13833022ffbec17c1a9455417bab0045212b85ef
Hayden Pennington <haydenpennington@icloud.com>
2023-05-09 22:48:38 -0500

    remove comment

--- iOS/GlobalDimensionIOS.swift
+++ iOS/GlobalDimensionIOS.swift
@@ -10,7 +10,7 @@ import Foundation
 struct GlobalDimensionIOS {
     static let treeWidth: CGFloat = 350
     static let toolbarWidth = Spacing.spacing4.rawValue + (Spacing.spacing3.rawValue * 2)
-//    static let maxDocumentContentWidth = 800.0
+    static let maxDocumentContentWidth = 800.0
 //    static let minDocumentContentWidth = 200.0
     static let minDocumentContentHeight = 300.00
     static let iconSmall = 16.0
@@ -20,5 +20,4 @@ struct GlobalDimensionIOS {
     static let maxDocumentPreviewContentWidth = 450.0
     static let labelModalHeight = 500.0
     static let labelModalWidth = 400.0
-    static let maxDocumentContentWidth = 800.0
 }
